2f3ff9bbf58231f7d93ba0b67fd5216d6e5ca199
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-06-10 10:45:01 +0700

    Configs

---
+++ src/configs/global.js
@@ -0,0 +1,10 @@
+const config = {
+    mysql: {
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
+    }
+}
+
+module.exports = config;
