75e60013fd1571d37ad300a5fa8ce4576d5d8b70
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-06-10 11:10:56 +0700

    routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productRouter = require('./library');
+const libraryRouter = require('./library');
 
 router.use('/library', libraryRouter);
 
