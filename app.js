297ea36e1db34e73b8ee7ddd79a1f5fd94704753
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-06-10 10:40:12 +0700

    Connection

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const connection = require("./src/helpers/mysql");
+const routes = require("./src/routes/index");
+
+connection.connect(function (error) {
+  if (error) throw error;
+  console.log("Database Has Conenected!");
+});
+
+app.use(morgan("dev"));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+app.use("/", routes);
+
+app.listen(3000, function () {
+  console.log("Library Apps Runing At Port:3000!");
+});
